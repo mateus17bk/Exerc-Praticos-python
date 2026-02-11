8ec5a02a4fec02c307cfeeb76eb2b2bb60015be3
MATEUS GUEDES <mateusguedes17@gmail.com>
2026-02-11 15:17:44 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,20 +1,57 @@
+1)
 print("Olá mundo")
 
 ###
 
+2)
 nome = input("Informe do seu nome: ")
 print("O seu nome é " + nome)
 
 ###
 
+3)
 nome = input('Digite seu nome: ')
 idade = input('Digite sua idade: ')
 print("O seu nome é " + nome + " e sua idade é " + idade)
 
 ###
 
+4)
 num = input("Digite um número: ")
 print("O númerero é: " + num)
 
 ###
+
+5)
+
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+num3 = int(input("Digite o terceiro número: "))
+print("A soma dos números total é: ", int(num1+num2+num3))
+
+
+###
+
+5)
+
+###
+
+5)
+
+###
+
+5)
+
+###
+
+5)
+
+###
+
+5)
+
+###
+
+5)
+
 
