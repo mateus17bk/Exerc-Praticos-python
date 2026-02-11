1220e9bd90e2834eb638d22a3ad8428761f3dedb
MATEUS GUEDES <mateusguedes17@gmail.com>
2026-02-11 11:40:26 -0300

    Create index.py

---
+++ index.py
@@ -0,0 +1,20 @@
+print("Olá mundo")
+
+###
+
+nome = input("Informe do seu nome: ")
+print("O seu nome é " + nome)
+
+###
+
+nome = input('Digite seu nome: ')
+idade = input('Digite sua idade: ')
+print("O seu nome é " + nome + " e sua idade é " + idade)
+
+###
+
+num = input("Digite um número: ")
+print("O númerero é: " + num)
+
+###
+
