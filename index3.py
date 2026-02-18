b0c85087fc2532c1dad11698616ae0e2944ef150
MATEUS GUEDES <mateusguedes17@gmail.com>
2026-02-18 09:05:58 -0300

    Update index3.py

--- index3.py
+++ index3.py
@@ -57,6 +57,11 @@ for exibir in range(num_inicio,num_fim):
     else:
         print()
 
+# 9) Faça um algoritmo que imprima a frase "estou em looping" e, em seguida, solicite ao usuário digitar uma letra. Caso a letra seja o "q" finalize a aplicação. Do contrário, imprima novamente a mesma frase até que o caractere "q" seja enviado:
+i = input("Informe a letra: ")
+while i != "q":
+    print("Estou em loop")
+print("finalizei o loop")
 
 
 
